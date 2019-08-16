49abef2aa006e3f6050141497e242ca9759df4a9
marcos <marcos@ultrapcifmg>
2019-08-16 10:04:14 -0300

    Update example

---
+++ algorithms/example_soccer/db.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS players;
+
+CREATE TABLE players (
+  name TEXT,
+  function TEXT,
+  goals INTEGER,
+  league TEXT
+);
+
+INSERT INTO players VALUES
+('Messi',   'attack',   25, 'spanish'  ),
+('Ribeiro', 'midfield', 21, 'spanish'  ),
+('Oscar',   'midfield', 15, 'spanish'  ),
+('Soares',  'attack',   14, 'spanish'  ),
+('Silva',   'defense',   9, 'brazilian');
---
+++ algorithms/example_travels/db.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS travels;
+
+CREATE TABLE travels (
+  destination  TEXT,
+  price  INTEGER,
+  duration  INTEGER,
+  itinerary  TEXT
+);
+
+INSERT INTO travels VALUES
+('Angra'         , 2000, 4, 'cruise'),
+('Buzios'        , 2000, 5, 'beach'),
+('Salvador'      , 2600, 6, 'cruise'),
+('Belo Horizonte', 2700, 5, 'urban'),
+('Rio de Janeiro', 2600, 7, 'beach');
