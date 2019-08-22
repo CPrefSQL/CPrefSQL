a3e81c6f1c48404693f8b001c6ad3f1ab9666689
Lucas <lucasmarianovieira@gmail.com>
2019-08-21 22:55:56 -0300

    Added new example with smartphones

---
+++ algorithms/example_smartphones/create_db.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sqlite3 db.db3 ".read db.sql"
---
+++ algorithms/example_smartphones/db.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS smartphones;
+
+CREATE TABLE smartphones (
+    brand TEXT,
+    model TEXT,
+    ram FLOAT,
+    storage FLOAT,
+    screen FLOAT, 
+    price DECIMAL(10,2)
+);
+
+INSERT INTO smartphones VALUES
+('Apple', 'Iphone X', 3.0, 64.0, 5.8, 4600),
+('Samsung', 'Galaxy S10', 8.0, 128.0, 6.1, 3200),
+('Samsung', 'Galaxy Note 9', 6.0, 128.0, 6.4, 2600),
+('Xiaomi', 'MI 9', 6.0, 64.0, 6.4, 2200),
+('Samsung', 'Galaxy S8', 4.0, 64.0, 5.8, 2300),
+('Motorola', 'Moto X4', 3.0, 32.0, 5.2, 1400);
