e7dcc65ac4f24496869d340490e59aefeea19994
Lucas <lucasmarianovieira@gmail.com>
2019-08-21 22:28:15 -0300

    Adding support for the new method based on preference level of formulas

---
+++ algorithms/test_formulas.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Module for best algorithm testing
+'''
+
+import os
+import sys
+import sqlite3
+
+# Required to relative package imports
+PATH = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.realpath(os.path.join(PATH, '..')))
+
+
+if __name__ == '__main__':
+    from algorithms.formulas_method import get_formulas_best, get_formulas_topk
+    
+    if len(sys.argv) != 4:
+        exit(0)
+    PREF_FILE = open(sys.argv[1])
+    PREF_TEXT = PREF_FILE.read()
+    DATA_FILE = sys.argv[2]
+    DATA_TABLE = sys.argv[3]
+    REC_LIST = []
+    print '\n\nPreferences:'
+    print PREF_TEXT
+    CON = sqlite3.connect(DATA_FILE)
+    CON.row_factory = sqlite3.Row
+    CURSOR = CON.cursor()
+    CURSOR.execute('SELECT * FROM ' + DATA_TABLE + ';')
+    print '\n\nInput records:'
+    for rec in CURSOR.fetchall():
+        REC_LIST.append(dict(rec))
+        print dict(rec)
+        
+    print '\n\nBest records:'
+    BEST_LIST = get_formulas_topk(PREF_TEXT, REC_LIST,4)
+    #BEST_LIST = get_formulas_best(PREF_TEXT, REC_LIST)
+    for rec in BEST_LIST:
+        print rec
+                
+            
+            
+        
+            
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ preference/btg_graph.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+'''
+Module to build preference graphs
+
+This graphs are used in theory consistency tests
+'''
+
+class BTG_Graph(object):
+    '''
+    Better Than Graph (BTG) Graph
+
+    '''
+
+    # Dictionary do store vertex and edges
+    _graph_dict = {}
+
+    def __init__(self):
+        '''
+        Initializes a graph object
+        '''
+        # Dictionary to store vertices and edges
+        self._graph_dict = {}
+
+    def __str__(self):
+        return str(self._graph_dict)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __del__(self):
+        self._graph_dict.clear()
+        del self._graph_dict
+
+    def add_edge(self, from_vertex, to_vertex):
+        '''
+        Add an edge from 'from_vertex' to 'to_vertex'
+
+        If vertices don't exist, they will be created
+        '''
+        
+        #hash dictionaries the lazy way
+        #from_vertex=str(from_vertex)
+        
+        if to_vertex is not None:
+            #to_vertex  =str(to_vertex)
+            
+            
+            if from_vertex not in self._graph_dict:
+                self._graph_dict[from_vertex] = []
+            if to_vertex not in self._graph_dict:
+                self._graph_dict[to_vertex] = []
+            if to_vertex not in self._graph_dict[from_vertex]:
+                self._graph_dict[from_vertex].append(to_vertex)
+        else:
+            if from_vertex not in self._graph_dict:
+                self._graph_dict[from_vertex] = []
+        
+    def get_top_vertex(self):
+        nodes=set()
+        
+        for index_node in self._graph_dict:
+            nodes.add(index_node)
+        
+        for index_node in self._graph_dict:
+            for node in self._graph_dict[index_node]:
+                if node in nodes:
+                    nodes.remove(node)
+        
+        return nodes
+
